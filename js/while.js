5388cc87b441b8f55eaf260037afaa3e7b1f5937
Lex Zavala <lex.zavala7@gmail.com>
2021-03-25 16:45:46 -0500

    added code to while js file, initially it lived inside the lecture file

--- js/while.js
+++ js/while.js
@@ -5,3 +5,22 @@
 //     num = num * 2;
 //     console.log("Previous number times 2 is " + num);
 // }
+
+// WHILE DO LOOPS
+
+// var allCones = Math.floor(Math.random() * 50) +50;
+// console.log("Cones I started the day with " + allCones);
+// do {
+//     var conesSold = Math.floor(Math.random() * 5) + 1;
+//
+//     if (conesSold > allCones){
+//         alert("Cannot sell you " + conesSold + " I only have " + allCones + " left!");
+//         console.log("Cannot sell you " + conesSold + " I only have " + allCones + " left!");
+//     } else {
+//         alert ("I have " + allCones + " and I am selling you " + conesSold);
+//         var allCones = allCones - conesSold;
+//         console.log("sold " + conesSold + " cones");
+//         console.log("Cones after sale " + allCones);
+//     }
+// } while (allCones > 0);
+//     alert("I am sorry, I sold all of my cones!");
