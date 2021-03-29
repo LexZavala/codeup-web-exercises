f6fcbc19b3b38b0fb56147631277adfa62b4261d
Lex Zavala <lex.zavala7@gmail.com>
2021-03-29 10:22:25 -0500

    completed the pyramid number exercise on for looops

--- js/for_loops.js
+++ js/for_loops.js
@@ -17,9 +17,25 @@ showMultiplicationTable(7);
 
 for (var i = 0; i <= 10; i++){
     var randomNum = Math.floor(Math.random() * 20) + 180;
-    if (randomNum % 2 == 0){
+    if (randomNum % 2 === 0){
         console.log(randomNum + " is even!");
     } else {
         console.log(randomNum + " is odd!");
     }
+}
+
+// for (var i =1; i <= 9; i++){
+//     console.log(i);
+//     while ( i <= 999999999){
+//         i * 11
+//     }
+// }
+
+for(var i=1; i<=9; i++){
+    var num ='';
+    for(var x =0; x <i; x++){
+        num += i;
+        ;
+    }
+    console.log(num)
 }
