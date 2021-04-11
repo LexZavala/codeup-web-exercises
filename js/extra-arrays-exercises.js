8078c583507946125aa46df21460c9c903bcc372
Lex Zavala <lex.zavala7@gmail.com>
2021-04-10 23:51:35 -0500

    revised some code to solve for the second exercise

--- js/extra-arrays-exercises.js
+++ js/extra-arrays-exercises.js
@@ -17,15 +17,18 @@ allIndexesOf(fruits, "pineapple");
 
 var bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
 
-function removeAll (arr, value){
-    for(var i = 0; i < arr.length; i++){
-        var index = arr.indexOf(value);
-        if(value === arr[i]){
-            var newArr = arr.slice(index, 2);
-        }
-    } console.log(newArr);
-}
-removeAll(bugs, "ant");
-removeAll(bugs, "mosquito");
-removeAll(bugs, "roach");
+// function removeAll (arr, value){
+//     for(var i = 0; i < arr.length; i++){
+//         var index = arr.indexOf(value);
+//         if(value === arr[i]){
+//             var newArr = arr.slice(index, 2);
+//         }
+//     } console.log(newArr);
+// }
+
+function removeAll(arr, value) { return arr.filter(function(ele){ return ele !== value; });}
+
+console.log(removeAll(bugs, "ant"));;
+console.log(removeAll(bugs, "mosquito"));;
+console.log(removeAll(bugs, "roach"));;
 
