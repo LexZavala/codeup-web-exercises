70d7073260c5b77552fc13ec8b11beedbf7be30d
Lex Zavala <lex.zavala7@gmail.com>
2021-05-10 11:59:56 -0500

    modified the ajax call to console log dates

--- js/weather_map_js.js
+++ js/weather_map_js.js
@@ -1,5 +1,8 @@
 "use strict";
 
-$.ajax("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=" + WEATHER_MAP_TOKEN).done(function (resp){
+$.ajax("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely,current&appid=" + WEATHER_MAP_TOKEN).done(function (resp){
     console.log(resp);
+    var today = resp.daily[0]
+    var todayDate = new Date(today.dt * 1000);
+    console.log(todayDate);
 });
