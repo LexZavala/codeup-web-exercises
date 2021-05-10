556eeb675b31b86e5f34e5dd88d510f41d97dfe3
Lex Zavala <lex.zavala7@gmail.com>
2021-05-10 12:34:29 -0500

    fixed ajax request with API

--- js/weather_map_js.js
+++ js/weather_map_js.js
@@ -1,8 +1,18 @@
 "use strict";
 
-$.ajax("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely,current&appid=" + WEATHER_MAP_TOKEN).done(function (resp){
+$.ajax("https://api.openweathermap.org/data/2.5/onecall" , {
+    data: {
+        APPID: WEATHER_MAP_TOKEN,
+        lon: -98.49,
+        lat: 29.42,
+        exclude: "minutely"
+    }
+}).done(function (resp){
     console.log(resp);
-    var today = resp.daily[0]
-    var todayDate = new Date(today.dt * 1000);
-    console.log(todayDate);
 });
+//     ?lat=33.44&lon=-94.04&exclude=hourly,minutely,current&appid=" + WEATHER_MAP_TOKEN).done(function (resp){
+//     console.log(resp);
+//     var today = resp.daily[0]
+//     var todayDate = new Date(today.dt * 1000);
+//     console.log(todayDate);
+// });
