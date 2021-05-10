4f048ab1a735a6c37b3b9328529347d8b8f5d52d
Lex Zavala <lex.zavala7@gmail.com>
2021-05-10 17:14:30 -0500

    completed functionality to render a singleday's worth of weather data

--- js/weather_map_js.js
+++ js/weather_map_js.js
@@ -10,8 +10,16 @@ $.ajax("https://api.openweathermap.org/data/2.5/onecall" , {
     }
 }).done(function (resp){
     console.log(resp);
+    // Date sorting variables
     var today = resp.daily[0];
     var todayDate = new Date(today.dt * 1000);
+    var shortDayName = todayDate.toLocaleString('en-us', {weekday: 'long'});
+    var shortMonth = todayDate.toLocaleString('en-us', {month: 'long'});
+    var shortDayNum = todayDate.toLocaleString('en-us', {day: 'numeric'});
+    var shortYear = todayDate.toLocaleString("en-US", {year: "numeric"})
+    var organizedDate = shortDayName + ", " + shortMonth + " " + shortDayNum + " " + shortYear;
+
+    // Rest of variables
     var todayOverallTemp = today.temp;
     var currentTemp = todayOverallTemp.day.toFixed() + "°F";
     var maxTemp = todayOverallTemp.max.toFixed() + "°F";
@@ -19,7 +27,17 @@ $.ajax("https://api.openweathermap.org/data/2.5/onecall" , {
     var todayDescription = today.weather[0].description;
     var humidity = today.humidity + "%";
     var feelsLike = today.feels_like;
-    var feelsLikeTemp = feelsLike.day + "°F";
+    var feelsLikeTemp = feelsLike.day.toFixed() + "°F";
+
+    function renderWeather() {
+        $('#date').html(organizedDate);
+        $('#description').html(todayDescription);
+        $('#temperature').html(currentTemp);
+        $('#maxAndMin').html("Max: "+ maxTemp + " Min: " + minTemp);
+        $('#feelsLike').html("Feels like: " + feelsLikeTemp);
+    }
+
+    renderWeather();
 
     console.log(todayDate);
     console.log(todayOverallTemp);
@@ -29,9 +47,22 @@ $.ajax("https://api.openweathermap.org/data/2.5/onecall" , {
     console.log(feelsLike);
     console.log(feelsLikeTemp);
 });
+
 //     ?lat=33.44&lon=-94.04&exclude=hourly,minutely,current&appid=" + WEATHER_MAP_TOKEN).done(function (resp){
 //     console.log(resp);
 //     var today = resp.daily[0]
 //     var todayDate = new Date(today.dt * 1000);
 //     console.log(todayDate);
 // });
+
+// var date = new Date(dailyForecast.dt * 1000);
+// var human = date.toLocaleString();
+// console.log(human);
+// var humanDay = todayDate.toLocaleString('en-us', {weekday: 'long'});
+// console.log(humanDay);
+// var humanMonth = todayDate.toLocaleString('en-us', {month: 'long'});
+// console.log(humanMonth);
+// var humanDate = todayDate.toLocaleString('en-us', {day: 'numeric'});
+// console.log(humanDate)
+// var humanYear = todayDate.toLocaleString("en-US", {year: "numeric"})
+// console.log(humanYear)
