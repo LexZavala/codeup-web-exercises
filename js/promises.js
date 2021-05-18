cf0005343ddf987c0a95bec9252f3618550b3273
Lex Zavala <lex.zavala7@gmail.com>
2021-05-18 09:38:32 -0500

    completed exercises on promises

--- js/promises.js
+++ js/promises.js
@@ -17,9 +17,29 @@ function githubUsername (userName) {
 
             console.log(`Last date of push was ${cleanDate[0]}`)
         })
+        .catch((resp => alert("error 300, fetch failed")))
 }
 
 githubUsername('LexZavala')
 
 
 // Events public is the right URL, INDEX 0 of the events is your last push, inside of that event there is a "payload", that has an array of "commits" and the index 0 of that  is the last commit
+
+function wait (time) {
+    const myPromise = new Promise(function(resolve, reject) {
+        if (time.toString().length < 3) {
+        setTimeout(resolve, time * 1000);
+        } else {
+            setTimeout(resolve, time);
+        }
+    }).then(function() {
+        console.log(time.toString().length);
+        if (time.toString().length < 3){
+            console.log(`Promise fulfilled after ${time} seconds`);
+        } else {
+            console.log(`Promise fulfilled after ${time} milliseconds`);
+        }
+    });
+}
+
+wait(2000);
